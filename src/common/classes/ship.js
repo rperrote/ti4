9d53854cfc3d5cee6e9c7504097113e0fee0242f
Rodrigo Perrote <rodrigoperrote@gmail.com>
2018-01-20 15:36:37 +0000

    Comiteo para laburar en mi pc

---
+++ src/common/classes/ship.js
@@ -0,0 +1,12 @@
+class Ship {
+    constructor (name, cost, battle, mvmt, especial) {
+        this.name = name;
+        this.protein = protein;
+        this.carbs = carbs;
+        this.fat = fat;
+        this.fat = fat;
+        
+    }
+}
+
+export default Ship;
